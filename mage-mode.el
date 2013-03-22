cf227684fcef2c824f0cbc662c839d6701214621
Daniel <daniel.r.ness@gmail.com>
2013-03-22 13:57:54 +0000

    Initial commit

---
+++ mage-mode.el
@@ -0,0 +1,15 @@
+;;
+;; -- Mage-mode --
+;; @author Daniel Ness <daniel.r.ness@gmail.com>
+;;
+
+;; -- Aims --
+;; 1. Creation of new module skeleton
+;; 2. Module search
+;; 3. Maintain a list of projects
+
+
+
+
+
+

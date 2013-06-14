682404bb2f4432df40d765f1db870499263abd61
Daniel <daniel.r.ness@gmail.com>
2013-06-14 17:02:20 +0100

    * Bridge script between magento and emacs * Version checking with mage-get-version

--- magemacs.el
+++ magemacs.el
@@ -6,12 +6,12 @@
 ;; -- Aims --
 ;; 1. Creation of new module skeleton
 ;; 2. Module search
-;; 3. Maintain a list of projects
+
+(setq magemacs-dir (expand-file-name (file-name-directory (buffer-file-name))))
 
 (defun mage-get-root ()
   ;; Get root magento installation directory that is parent of file
   ;; by attempting to identify {root}/app/Mage.php
-  (interactive)
   (let* ((dir (file-name-directory (buffer-file-name)))
          (root-dir (expand-file-name (locate-dominating-file dir "app"))))
     (if (stringp root-dir)
@@ -34,5 +34,50 @@
 
 (defun mage-list-modules ()
   (directory-files (mage-get-module-dir) nil "\\w+_\\w+.xml$"))
+
+(defun mage-new-module ()
+  (interactive)
+  (read-from-minibuffer "Codepool (local/community): ")
+  (read-from-minibuffer "Namespace_ModuleName: ")
+  )
+
+(defun mage-describe-model ()
+  (interactive)
+  )
+
+(defun mage-summary ()
+  (interactive)
+  )
+
+(defun mage-version ()
+  (interactive)
+  )
+
+(defun mage-list-modules ()
+  (interactive)
+  )
+
+(defun mage-get-version ()
+  (interactive)
+  (message (mage-run-shell "version")))
+
+(defun mage-run-shell (arg)
+  (let ((root-dir (mage-get-root)))
+    (with-temp-buffer
+      (let ((output (current-buffer)))
+        (progn
+          (with-temp-buffer
+            (insert-file-contents (filepath-concat magemacs-dir "magemacs.php"))
+            (call-process "php" nil output nil "-r" (buffer-string) arg)
+            )
+          (buffer-string))))))
+
+                          
+
+
+
+
+
+
 
 
---
+++ magemacs.php
@@ -0,0 +1,50 @@
+require_once 'abstract.php';
+
+class Magemacs_Shell extends Mage_Shell_Abstract
+{
+
+    protected function _isEnterprise()
+    {
+        return Mage::getConfig()->getModuleConfig('Enterprise_Enterprise')
+            && Mage::getConfig()->getModuleConfig('Enterprise_AdminGws') 
+            && Mage::getConfig()->getModuleConfig('Enterprise_Checkout') 
+            && Mage::getConfig()->getModuleConfig('Enterprise_Customer');
+    }
+
+    protected function _isProfessional()
+    {
+        return Mage::getConfig()->getModuleConfig('Enterprise_Enterprise') 
+            && !Mage::getConfig()->getModuleConfig('Enterprise_AdminGws') 
+            && !Mage::getConfig()->getModuleConfig('Enterprise_Checkout') 
+            && !Mage::getConfig ()->getModuleConfig('Enterprise_Customer');
+    }
+
+    protected function _isCommunity()
+    {
+        return !$this->_isEnterprise() && !$this->_isProfessional();
+    }
+
+    protected function _checkVersion()
+    {
+        $edition = "Community";
+
+        if($this->_isEnterprise()) {
+            $edition = "Enterprise";
+        } elseif($this->_isProfessional()) {
+            $edition = "Professional";
+        }
+
+        return "$edition " . Mage::getVersion();
+    }
+
+    public function run()
+    {
+        if(isset($this->_args['version'])) {
+            echo $this->_checkVersion();
+        }
+    }
+    
+}
+
+$x = new Magemacs_Shell;
+$x->run();

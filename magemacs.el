6676f05c398322655fe5043042eb8f6e7c462ece
Daniel <daniel.r.ness@gmail.com>
2013-06-14 01:10:43 +0100

    Added some directory getters

--- magemacs.el
+++ magemacs.el
@@ -8,18 +8,31 @@
 ;; 2. Module search
 ;; 3. Maintain a list of projects
 
-(defun get-magento-root (file)
+(defun mage-get-root ()
   ;; Get root magento installation directory that is parent of file
   ;; by attempting to identify {root}/app/Mage.php
-  (let ((root-dir (locate-dominating-file dir "app")))
+  (interactive)
+  (let* ((dir (file-name-directory (buffer-file-name)))
+         (root-dir (expand-file-name (locate-dominating-file dir "app"))))
     (if (stringp root-dir)
         (let* ((dir (expand-file-name root-dir))
                (app-dir (concat (file-name-as-directory dir) (file-name-as-directory "app"))))
-          (if (file-exists-p (concat app-dir "Mage.php"))
-              dir)))))
+          (if (file-exists-p (filepath-concat app-dir "Mage.php"))
+              root-dir)))))
 
-(defun magemacs-mode ()
-  (interactive)
-  (let ((file (buffer-file-name)))
-    (message (get-magento-root file))))
+(defun filepath-concat (dir file)
+  (concat (file-name-as-directory dir) file))
+
+(defun mage-get-app-dir ()
+  (filepath-concat (mage-get-root) "app"))
+
+(defun mage-get-etc-dir ()
+  (filepath-concat (mage-get-app-dir) "etc"))
+
+(defun mage-get-module-dir ()
+  (filepath-concat (mage-get-etc-dir) "modules"))
+
+(defun mage-list-modules ()
+  (directory-files (mage-get-module-dir) nil "\\w+_\\w+.xml$"))
+
 
